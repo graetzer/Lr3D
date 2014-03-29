9e4a1e30aa7f0346fede1cc20b10825792fd0095
wasc <niko.souris@googlemail.com>
2014-03-29 20:53:18 +0100

    fix #5

---
+++ game/src/main/java/com/linerunner3d/Graphic.java
@@ -0,0 +1,8 @@
+package com.linerunner3d;
+
+/**
+ *
+ * Created by wasc on 2014-03-29.
+ */
+public class Graphic {
+}
--- game/src/main/java/com/linerunner3d/MainActivity.java
+++ game/src/main/java/com/linerunner3d/MainActivity.java
@@ -2,37 +2,37 @@ package com.linerunner3d;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.ContactsContract;
+import android.support.v4.view.GestureDetectorCompat;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends Activity {
 
+
+    private boolean isTouch = false;
+    private RelativeLayout mRL;
+    private GestureDetectorCompat gestureDetector = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        gestureDetector = new GestureDetectorCompat(this,new GestureInputDetector(this));
+
+
         setContentView(R.layout.activity_main);
+        mRL = (RelativeLayout) findViewById(R.id.mRL);
+        mRL.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
